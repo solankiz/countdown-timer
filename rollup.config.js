9a3518ce158dcdc5ef07b73428d70169408d67bd
Kiran Solanki <kiranmlvya@gmail.com>
2019-05-14 23:49:45 +0530

    version to 1.0.1 cn not defined issue

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
-import postcssModules from 'postcss-modules';
 
 let configArray = [];
 
