0d2a86c857073aa3add1ad41fee90c42c5fce836
Kiran Solanki <kiranmlvya@gmail.com>
2019-05-14 23:16:35 +0530

    First release

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import resolve from 'rollup-plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import postcssModules from 'postcss-modules';
+
+let configArray = [];
+
+const createConfig = (type) => ({
+    input: 'src/index.js',
+    external: ['react'],
+    output: {
+        file: `dist/countdown.${type}.js`,
+        format: type,
+        name: 'countdown',
+        esModule: type !== 'umd',
+        globals: type === 'umd' ? { react: "React" } : undefined
+    },
+    plugins: [
+        resolve(),
+        postcss({
+            extract: 'dist/styles.css',
+        }),
+        babel({
+            exclude: "node_modules/**"
+        }),
+        type === 'umd' && terser()
+    ]
+});
+
+configArray.push(createConfig(process.env.NODE_ENV));
+
+export default [ ...configArray ];
